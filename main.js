08da8ef8dac465ac6dae8eb4763d7b84670520bf
Sergey Meshchankin <smeshchankin@gmail.com>
2020-09-08 23:44:21 +0300

    [NFS-002] Game handlers

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 (function() {
+    const element = {
+        score: document.querySelector('.score'),
+        start: document.querySelector('.start'),
+        area: document.querySelector('.area')
+    };
+
+    element.score.addEventListener('click', startGame);
+    document.addEventListener('keydown', accelerate);
+    document.addEventListener('keyup', stop);
+
+    function startGame() {
+        element.start.classList.add('hide');
+    }
+
+    function accelerate(event) {
+        event.preventDefault();
+    }
+
+    function stop(event) {
+        event.preventDefault();
+    }
 }());
