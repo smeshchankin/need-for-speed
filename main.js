923c7da5362797a2d03a5cd9701a6b1a31abf2b8
Sergey Meshchankin <smeshchankin@gmail.com>
2020-09-13 23:27:05 +0300

    [NFS-004] Car moving

--- main.js
+++ main.js
@@ -27,13 +27,33 @@
 
     function startGame() {
         element.start.classList.add('hide');
-        settings.start = true;
         element.area.appendChild(element.car);
+
+        settings.start = true;
+        settings.x = element.car.offsetLeft;
+        settings.y = element.car.offsetTop;
+
         requestAnimationFrame(playGame);
     }
 
     function playGame() {
         if (settings.start) {
+            if (keys.ArrowLeft) {
+                settings.x -= settings.speed;
+            }
+            if (keys.ArrowRight) {
+                settings.x += settings.speed;
+            }
+            if (keys.ArrowUp) {
+                settings.y -= settings.speed;
+            }
+            if (keys.ArrowDown) {
+                settings.y += settings.speed;
+            }
+
+            element.car.style.left = settings.x;
+            element.car.style.top = settings.y;
+
             requestAnimationFrame(playGame);
         }
     }
